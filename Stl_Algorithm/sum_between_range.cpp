de96189fb41ebf91c5ab5e1422a5b8106655c509
Abhinav <guptaabhinav707@gmail.com>
2021-07-26 12:32:39 +0530

    added sum between the range stl algo

---
+++ Stl_Algorithm/sum_between_range.cpp
@@ -0,0 +1,28 @@
+/*
+ This stl algo is used to find sum between the specified range of the iterable container
+
+ Function: accumulate()
+ Header file: numeric
+ */
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include<numeric>
+using namespace std;
+
+int main()
+{
+    // Syntax:
+    // accumulate(begin_iterator, end_iterator, initial_sum)
+
+    int arr[] = {5, 3, 6, 9, 10, 25, 3};
+    vector<int> vec = {5, 2, 6, 9, 10, 4, 3};
+
+    int sum=accumulate(arr,arr+7,0);
+    cout<<"array sum is: "<<sum<<endl;
+    
+    sum=accumulate(vec.begin(), vec.end(),0);
+    cout<<"vec sum is: "<<sum<<endl;
+
+    return 0;
+}
