9bcacab4f89380341925290ba6cce37498209f5b
Abhinav <guptaabhinav707@gmail.com>
2021-07-29 09:46:12 +0530

    basic template v:1.0

---
+++ CP_Template/template.cpp
@@ -0,0 +1,24 @@
+// include all header files in one go!!
+#include <bits/stdc++.h>
+using namespace std;
+
+// will be called from the main to solve the proble ;
+void solve()
+{
+}
+
+int main()
+{
+    // fast input and output method
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // number of test cases
+    long long int t;
+    cin >> t;
+    while (t--)
+    {
+        // solve each test case 
+        solve();
+    }
+    return 0;
+}
