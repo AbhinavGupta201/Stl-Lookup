09322a917432d87e3815c1ecdd61a6a936f362df
Abhinav <guptaabhinav707@gmail.com>
2021-08-02 14:26:22 +0530

    Recursion added

---
+++ Recursion/NQueen.cpp
@@ -0,0 +1,81 @@
+/* Problem Statement;
+Given a n*n board. and n queens you have to place the queens.
+Rules:
+    1. No queen attack each other
+    2. Each row and column must have exactly one queen.
+    
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+bool isSafe(ll row, ll col, ll n, vector<vector<ll>> &vec)
+{
+    // row check is safe
+    for (ll i = row - 1; i >= 0; i--)
+    {
+        if (vec[row][col] == 1)
+            return false;
+    }
+
+    // check it upper diagonal
+    for (ll i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+    {
+        if (vec[row][col] == 1)
+            return false;
+    }
+
+    // check in the lower diagonal
+    for (ll i = row +1, j = col - 1; i<n && j >= 0; i++, j--)
+    {
+        if (vec[row][col] == 1)
+            return false;
+    }
+    return true;
+}
+void NQueen(ll col, ll n, vector<vector<ll>> &vec)
+{
+    // base case:
+    if (col == n)
+    {
+        for (auto it : vec)
+        {
+            for (auto e : it)
+            {
+                cout << e << " ";
+            }
+            cout << endl;
+        }
+        return;
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        if (isSafe(i, col, n,vec))
+        {
+            vec[i][col] = 1;
+            NQueen(col + 1, n, vec);
+        }
+    }
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<vector<ll>> vec(n, vector<ll>(0));
+    NQueen(0, n, vec);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
